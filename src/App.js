6534bb04dcd79d510f3599816d22f94d9d3a3491
Julio Marques <juliomarquesjr@yahoo.com.br>
2021-09-07 20:17:14 -0300

    Commit Inicial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Formulario from './components/Formulario/';
+import TabelaNotas from './components/TabelaNotas/';
+import './assets/App.css';
+import './assets/css/bootstrap.min.css'
+import Logo from "./assets/img/reactjs.png"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    //Propriedade especial do React
+    this.state = {
+      notas: []
+    }
+  }
+
+  criarNota(titulo, texto) {
+    //Criar um array com a nova nota
+    const NovaNota = { titulo, texto };
+
+    //Cria um novo array com as informações anteriores e as novas
+    const novoArray = [...this.state.notas, NovaNota];
+
+    //Atributo para atualizar o estado do this.state passando o array atualizado, contanenado.
+    const novoEstado = {
+      notas: novoArray
+    }
+    //Atributo Especial do React
+    this.setState(novoEstado)
+
+    console.log(this.state.notas);
+  }
+
+  render() {
+    return (
+      <section>
+        <div className="container py-4">
+          <header class="pb-3 mb-4 border-bottom">
+            <a href="/" class="d-flex align-items-center text-dark text-decoration-none">
+            <img src={Logo} width={32} height={32}/>
+              <span class="fs-4">React.js - Cadastro de Notas</span>
+            </a>
+          </header>
+          <Formulario passandoCriarNota={this.criarNota.bind(this)} />
+          <TabelaNotas passandoNotas={this.state.notas} />
+        </div>
+      </section>
+    );
+  }
+
 }
 
 export default App;
